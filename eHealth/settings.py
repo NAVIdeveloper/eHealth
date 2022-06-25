1b3caa08cf62b6ac233eed44d36027223c1cc425
Navideveloper <pythondarslar@gmail.com>
2022-06-25 17:17:16 +0300

    Server2

--- eHealth/settings.py
+++ eHealth/settings.py
@@ -128,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = [BASE_DIR/'static/']
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / MEDIA_URL
 
